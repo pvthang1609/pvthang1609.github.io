3413d1c2858c06ab311ff60583949b43cc5c1912
pvthang1609 <58507978+pvthang1609@users.noreply.github.com>
2020-07-28 21:31:45 +0700

    weather

--- smallProject/weather/main.js
+++ smallProject/weather/main.js
@@ -1,10 +1,17 @@
-
 //select
-var selectToday = document.querySelector('.today')
-var selectTime = document.querySelector('.time')
-var selectDaysOfTheWeek = document.querySelector('.daysOfTheWeek')
-var selectIconWeather_image = document.querySelector('.iconWeather_image')
-var selectTemp = document.querySelector('.temp')
+var selectToday = document.querySelector('.today');
+var selectTime = document.querySelector('.time');
+var selectDaysOfTheWeek = document.querySelector('.daysOfTheWeek');
+var selectIconWeather_image = document.querySelector('.iconWeather_image');
+var selectTemp = document.querySelector('.temp');
+var selectWeatherOnTime_time1 = document.querySelector('.weatherOnTime_time1');
+var selectWeatherOnTime_time2 = document.querySelector('.weatherOnTime_time2');
+var selectWeatherOnTime_iconWeather_image0 = document.querySelector('.weatherOnTime_iconWeather_image0')
+var selectWeatherOnTime_iconWeather_image1 = document.querySelector('.weatherOnTime_iconWeather_image1')
+var selectWeatherOnTime_iconWeather_image2 = document.querySelector('.weatherOnTime_iconWeather_image2')
+var selectWeatherOnTime_temp0 = document.querySelector('.weatherOnTime_temp0')
+var selectWeatherOnTime_temp1 = document.querySelector('.weatherOnTime_temp1')
+var selectWeatherOnTime_temp2 = document.querySelector('.weatherOnTime_temp2')
 
 var arrDay = ['Chủ nhật', 'Thứ Hai', 'Thứ Ba', 'Thứ Tư', 'Thứ Năm', 'Thứ Sáu', 'Thứ Bảy']
 
@@ -20,8 +27,16 @@ setInterval(() => {
     var minute = today.getMinutes();
 
     selectTime.innerHTML = `${hour}:${minute}`;
-    selectToday.innerHTML = `Ngày ${date} tháng ${month}, năm ${year}`;
-    selectTemp.innerHTML = `${weather.temp}°C`
+    selectToday.innerHTML = `Ngày ${date} tháng ${month} năm ${year}`;
+
+    selectTemp.innerHTML = `${weather.temp}°C`;
+
+    selectWeatherOnTime_temp0.innerHTML = `${weather0.temp}°C`;
+    selectWeatherOnTime_temp1.innerHTML = `${weather1.temp}°C`;
+    selectWeatherOnTime_temp2.innerHTML = `${weather2.temp}°C`
+
+    selectWeatherOnTime_time1.innerHTML = `${arrDay[day + 2]}`
+    selectWeatherOnTime_time2.innerHTML = `${arrDay[day + 3]}`
 
     switch(day) {
         case 0:
@@ -55,8 +70,19 @@ const key = '6119700fea7a44ead023cd51337e6566';
 
 let weather = {
     temp: 0,
-    icon: '',
-    main: '',
+    icon: ''
+}
+let weather0 = {
+    temp: 0,
+    icon: ''
+}
+let weather1 = {
+    temp: 0,
+    icon: ''
+}
+let weather2 = {
+    temp: 0,
+    icon: ''
 }
 
 function getWeather(latitude, longitude) {
@@ -69,10 +95,22 @@ function getWeather(latitude, longitude) {
         return data;
     })
     .then( function(data) {
+        let today = new Date();
+        let day = today.getDay()
+
         weather.temp = data.current.temp - 273.15
         weather.icon = data.current.weather[0].icon
-        weather.main = data.current.weather[0].main
-        console.log(weather)
+
+        weather0.temp = Math.round(data.daily[day + 1].temp.day - 273.115)
+        weather0.icon = data.daily[day + 1].weather[0].icon
+
+        weather1.temp = Math.round(data.daily[day + 2].temp.day - 273.115)
+        weather1.icon = data.daily[day + 2].weather[0].icon
+
+        weather2.temp = Math.round(data.daily[day + 3].temp.day - 273.115)
+        weather2.icon = data.daily[day + 3].weather[0].icon
+
+        console.log(weather0, weather1, weather2)
     })
     .then( function pushWeather() {
         switch(weather.icon) {
@@ -111,7 +149,128 @@ function getWeather(latitude, longitude) {
                 selectIconWeather_image.src = 'icon/icon01/11d_n-thunderstorm.png'
                 break;        
         }
+        switch(weather0.icon) {
+            case '01d':
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/01d-clearSky.png'
+                break;
+            case '01n':
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/01n-clearSky.png'
+                break;
+            case "02d":
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/02d-fewClouds.png'
+                break;
+            case "02n":
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/02n-fewClouds.png'
+                break;
+            case "03d":
+            case "03n":
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/03d_n-scatteredClouds.png'
+                break;
+            case "04d":
+            case "04n":
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/04d_n-brokenClouds.png'
+                break;
+            case "09d":
+            case "09n":
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/09d_n-showerRain.png'
+                break;
+            case "10d":
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/10d-rain.png'
+                break;
+            case "10n":
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/10n-rain.png'
+                break;
+            case "11d":
+            case "11n":
+                selectWeatherOnTime_iconWeather_image0.src = 'icon/icon01/11d_n-thunderstorm.png'
+                break;        
+        }
+        switch(weather1.icon) {
+            case '01d':
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/01d-clearSky.png'
+                break;
+            case '01n':
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/01n-clearSky.png'
+                break;
+            case "02d":
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/02d-fewClouds.png'
+                break;
+            case "02n":
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/02n-fewClouds.png'
+                break;
+            case "03d":
+            case "03n":
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/03d_n-scatteredClouds.png'
+                break;
+            case "04d":
+            case "04n":
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/04d_n-brokenClouds.png'
+                break;
+            case "09d":
+            case "09n":
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/09d_n-showerRain.png'
+                break;
+            case "10d":
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/10d-rain.png'
+                break;
+            case "10n":
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/10n-rain.png'
+                break;
+            case "11d":
+            case "11n":
+                selectWeatherOnTime_iconWeather_image1.src = 'icon/icon01/11d_n-thunderstorm.png'
+                break;        
+        }
+        switch(weather2.icon) {
+            case '01d':
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/01d-clearSky.png'
+                break;
+            case '01n':
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/01n-clearSky.png'
+                break;
+            case "02d":
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/02d-fewClouds.png'
+                break;
+            case "02n":
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/02n-fewClouds.png'
+                break;
+            case "03d":
+            case "03n":
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/03d_n-scatteredClouds.png'
+                break;
+            case "04d":
+            case "04n":
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/04d_n-brokenClouds.png'
+                break;
+            case "09d":
+            case "09n":
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/09d_n-showerRain.png'
+                break;
+            case "10d":
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/10d-rain.png'
+                break;
+            case "10n":
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/10n-rain.png'
+                break;
+            case "11d":
+            case "11n":
+                selectWeatherOnTime_iconWeather_image2.src = 'icon/icon01/11d_n-thunderstorm.png'
+                break;        
+        }
     })
 }
-getWeather(20.9780671, 105.79381839999999);
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else{
+        alert('Định vị địa lý không được hỗ trợ bởi trình duyệt này.')
+    }
+}
+function showPosition(position) {
+    getWeather(position.coords.latitude, position.coords.longitude);
+}
+getLocation();
+
 
