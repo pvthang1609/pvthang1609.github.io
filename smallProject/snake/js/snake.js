ff160d163d71afe4530e2e081ab0752a9f98a71e
pvthang1609 <58507978+pvthang1609@users.noreply.github.com>
2020-07-28 19:33:50 +0700

    snake

--- smallProject/snake/js/snake.js
+++ smallProject/snake/js/snake.js
@@ -3,7 +3,22 @@ class snake {
         this.game = game;
         this.x = CANVAS_WIDTH / 2;
         this.y = CANVAS_HEIGHT /2;
+        this.listenMouseEvent();
     }
+    listenMouseEvent() {
+        this.game.canvas.addEventListener('mousemove', (event) => {
+            var rect = this.game.canvas.getBoundingClientRect();    //getBoundingClientRect => trả về 1 obj 'rect' bao gồm top, bot, left, right, x, y
+            this.processMouseEvent({
+                x: event.clientX - rect.left,
+                y: event.clientY - rect.top
+            });
+            console.log(event.offsetX, event.offsetY)
+        })
+    }
+    processMouseEvent(mousePos) {
+        console.log(mousePos)
+    }
+    
     update() {
 
     }
