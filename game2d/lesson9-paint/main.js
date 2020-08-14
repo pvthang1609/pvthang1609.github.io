3eb8e3951658b8f00cf1ad23ae223562e509e91e
pvthang1609 <pvthang1609@gmail.com>
2020-08-14 09:55:26 +0700

    paint

--- game2d/lesson9-paint/main.js
+++ game2d/lesson9-paint/main.js
@@ -4,6 +4,10 @@ let ctx = canvas.getContext('2d')
 let size = 5;
 let color = 'black'
 
+
+let selectTool__color = document.querySelectorAll('.tool__color');
+let selectTool__size = document.querySelectorAll('.tool__size');
+
 container.height = 800;
 container.width = 1200;
 canvas.height = 700;
@@ -39,5 +43,32 @@ function reset() {
     ctx.clearRect(0, 0, 1100, 1100)
 }
 
-
-//
+var activeColor = function() {
+    selectTool__color.forEach((button) => {
+            button.classList.remove('active');
+    });
+    event.target.classList.add('active');
+    switch (color) {
+        case 'black':
+            document.querySelector('.canvas').style.cursor = `url(./img/cursor-black.cur) 2 2, auto`
+            break;
+        case 'red':
+            document.querySelector('.canvas').style.cursor = `url(./img/cursor-red.cur) 2 2, auto`
+            break;
+        case 'yellow':
+            document.querySelector('.canvas').style.cursor = `url(./img/cursor-yellow.cur) 2 2, auto`
+            break;
+        case 'blue':
+            document.querySelector('.canvas').style.cursor = `url(./img/cursor-blue.cur) 2 2, auto`
+            break;
+        case 'blueviolet':
+            document.querySelector('.canvas').style.cursor = `url(./img/cursor-blueviolet.cur) 2 2, auto`
+            break;
+    }
+}
+var activeSize = function() {
+    selectTool__size.forEach((button) => {
+            button.classList.remove('active');
+    });
+    event.target.classList.add('active');
+}
