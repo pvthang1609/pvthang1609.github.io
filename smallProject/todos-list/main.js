d708cf05017c53182fe7839fdd595cecbd8a4ecf
pvthang1609 <58507978+pvthang1609@users.noreply.github.com>
2020-08-31 07:59:11 +0700

    todo list

--- smallProject/todos-list/main.js
+++ smallProject/todos-list/main.js
@@ -72,8 +72,113 @@ document.querySelector('.day__seven--date').innerHTML = `${dateSeven.getDate()}`
 
 //add todo
 let btAdd = document.querySelector('.btAdd')
+let infoDate = document.querySelector('.info__date')
+let sevenDay = document.querySelector('.sevenDay')
+let todosList = document.querySelector('.todosList')
+let creatNew = document.querySelector('.creatNew')
 
 btAdd.addEventListener('click', function() {
-  document.querySelector('.info__date').style.transform = 'translateX(-500px)'
-  // document.querySelector('.info__date').style.opacity = '0'
+  infoDate.style.visibility = 'hidden';
+  infoDate.style.opacity = '0';
+  sevenDay.style.visibility = 'hidden';
+  sevenDay.style.opacity = '0';
+  todosList.style.visibility = 'hidden';
+  todosList.style.opacity = '0';
+  creatNew.style.visibility = 'visible';
+  creatNew.style.opacity = '1';
 })
+
+//creat new
+let btCreat = document.querySelector('.btCreat')
+
+btCreat.addEventListener('click', function() {
+  infoDate.style.visibility = 'visible';
+  infoDate.style.opacity = '1';
+  sevenDay.style.visibility = 'visible';
+  sevenDay.style.opacity = '1';
+  todosList.style.visibility = 'visible';
+  todosList.style.opacity = '1';
+  creatNew.style.visibility = 'hidden';
+  creatNew.style.opacity = '0';
+  pushTodo()
+  selectCurrent()
+  selectHeading.value = ''
+  selectContent.value = ''
+  render()
+})
+
+//function tao id random
+var ID = function () {
+  return Math.random().toString(36).substr(2, 9);
+};
+
+for (let i = 1; i < 60; i++ ) {
+  document.querySelector('#minute').innerHTML += `<option value="${i}">${i}</option>`
+}
+
+function selectCurrent() {
+  document.querySelector('#date').selectedIndex = `${thisDate -1}`
+  document.querySelector('#month').selectedIndex = `${date.getMonth()}`
+  document.querySelector('#hour').selectedIndex = `${date.getHours() - 1}`
+  document.querySelector('#minute').selectedIndex = `${date.getMinutes() - 1}`
+
+}
+selectCurrent();
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+  document.querySelector('.delete').style.transform = 'translate(-50%, -50%) translateY(0px)'
+  btAdd.style.bottom = '-60px'
+  ev.target.style.opacity = '0.5'
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+
+}
+function drop(ev) {
+  var data = ev.dataTransfer.getData("text");
+  document.getElementById(data).remove()
+  document.querySelector('.delete').style.transform = 'translate(-50%, -50%) translateY(100px)'
+  btAdd.style.bottom = '30px'
+  arrTodo = arrTodo.filter(todo => todo.id !== data);
+}
+function cancel(ev) {
+  document.querySelector('.delete').style.transform = 'translate(-50%, -50%) translateY(100px)'
+  btAdd.style.bottom = '30px'
+  ev.target.style.opacity = '1'
+}
+class Todo {
+  constructor(id, date, time, heading, content,) {
+    this.id = id;
+    this.date = date;
+    this.time = time;
+    this.heading = heading;
+    this.content = content;
+  }
+}
+let selectDate = document.querySelector('#date')
+let selectMonth = document.querySelector('#month')
+let selectHour = document.querySelector('#hour')
+let selectMinute = document.querySelector('#minute')
+let selectHeading = document.querySelector('#heading')
+let selectContent = document.querySelector('#content')
+
+
+let arrTodo = []
+
+function pushTodo() {
+  let todo = new Todo(ID(), `${selectDate.value}-${selectMonth.value}`, `${selectHour.value}:${selectMinute.value}`,`${selectHeading.value}`, `${selectContent.value}`)
+  arrTodo.push(todo)
+}
+function render() {
+  document.querySelector('.innerTodoList').innerHTML = ``
+  for(let i = 0; i < arrTodo.length; i ++) {
+    document.querySelector('.innerTodoList').innerHTML += `<div class="todo" draggable="true" ondragstart="drag(event)" ondragend='cancel(event)' id="${arrTodo[i].id}">
+    <div class="todo__time">${arrTodo[i].time}</div>
+    <div class="todo__content">
+        <div class="todo__content--text heading">${arrTodo[i].heading}</div>
+        <div class="todo__content--text content">${arrTodo[i].content}</div>
+    </div>
+</div>`
+  }
+}
